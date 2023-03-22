990e07cc8fcee48d89d5b319edfa398ad510b27c
Nikita Petrykin <nvnedu2120@gmail.com>
2023-03-22 23:29:07 +0100

    feat: Added new endpoint in API
    - Added a new endpoint that returns two random matrices and their product
    - Added .dockerignore to prevent the virtualenv directory from being copied to the docker image
    - Updated dependencies (numpy)

--- python-project/spaceship/routers/api.py
+++ python-project/spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -15,3 +16,18 @@ def hello_world() -> dict:
     }
 
     return data
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+
+    product = np.dot(matrix_a, matrix_b)
+
+    result = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+
+    return result
